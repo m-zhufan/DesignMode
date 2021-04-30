5064c4ac82448fdb826fedf48f5ff1d72766a9ea
m-zhufan <m_zhuf@163.com>
2021-04-30 16:57:10 +0800

    update define

--- libs/Observer/observer_global.h
+++ libs/Observer/observer_global.h
@@ -20,7 +20,7 @@
 
 #if defined(_MSC_VER) || defined(WIN64) || defined(_WIN64) || defined(__WIN64__) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 #  define OBSERVER_DECL_EXPORT      __declspec(dllexport)
-#  define OBSERVER_DECL_EXPORT      __declspec(dllimport)
+#  define OBSERVER_DECL_IMPORT      __declspec(dllimport)
 #else
 #  define OBSERVER_DECL_EXPORT      __attribute__((visibility("default")))
 #  define OBSERVER_DECL_IMPORT      __attribute__((visibility("default")))
